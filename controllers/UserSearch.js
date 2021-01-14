ed46a31b9195757304d81feef7e445405848c2ae
Ana Caballero <ana.caballero@gmx.de>
2021-01-14 13:15:54 +0100

    add routes to create a user search and get a user search by user id

--- controllers/UserSearch.js
+++ controllers/UserSearch.js
@@ -1,34 +1,45 @@
-
 const { UserSearch } = require("../models");
 
-exports.getAllUserSearchs = async function (req, res) {
-const all = await UserSearch.find().limit(10);
-res.json(all);
+exports.createUserSearch = async function (req, res) {
+  const newUserSearch = await UserSearch.create(req.body);
+  res.json({ success: true, newUserSearch });
+};
+
+exports.getUserSearchByUserId = async function (req, res) {
+  const { userId } = req.params;
+  const userSearches = await UserSearch.find({ user_id: userId });
+  if (!userSearches) {
+    return res.status(404).send("User doesn't have any searches yet");
+  }
+  res.json(userSearches);
+};
+
+/* exports.getAllUserSearchs = async function (req, res) {
+  const all = await UserSearch.find().limit(10);
+  res.json(all);
 };
 
 exports.getUserSearchById = async function (req, res) {
-const { id } = req.params;
-const single = await UserSearch.findOne({ id: Number(id) });
-if (!single) {
+  const { id } = req.params;
+  const single = await UserSearch.findOne({ id: Number(id) });
+  if (!single) {
     return res.status(404).send("UserSearch with this ID does not exist");
-}
-res.json(single);
-};
-
-exports.createUserSearch = async function (req, res) {
-    const newUserSearch = await UserSearch.create(req.body);
-    res.json(newUserSearch);
+  }
+  res.json(single);
 };
 
 exports.updateUserSearch = async (req, res) => {
-    const { title, body, img } = req.body;
-    const { id } = req.params;
-    const result = await UserSearch.findByIdAndUpdate(id, req.body, {new:true})
-    res.json({ success: true, result });
-}
-
-exports.deleteUserSearch = async(req, res) => {
-    const { id } = req.params;
-    const result = await UserSearch.findByIdAndDelete()
-    res.sendStatus("204");
+  const { title, body, img } = req.body;
+  const { id } = req.params;
+  const result = await UserSearch.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.json({ success: true, result });
 };
+
+exports.deleteUserSearch = async (req, res) => {
+  const { id } = req.params;
+  const result = await UserSearch.findByIdAndDelete();
+  res.sendStatus("204");
+};
+*/
