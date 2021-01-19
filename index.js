7101e313a059501efbfdb1d36bafaa114a6c6c7a
SE-Nie <svenja.niehus@gmail.com>
2021-01-19 15:07:44 +0100

    test

--- index.js
+++ index.js
@@ -1,97 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const cookieParser = require("cookie-parser");
-//const session = require("express-session");
+const express = require('express')
+const app = express()
+
+var session = require('express-session')
 const cors = require("cors");
-const listEndpoints = require("express-list-endpoints");
-const colors = require("colors/safe");
-const { connectDB, connection } = require("./models");
+port = process.env.PORT || 4000
 
-const {
-  createUser,
-  getUser,
-  loginUser,
-  logoutUser,
-} = require("./controllers/user");
-const {
-  getUserSearchById,
-  getUserSearchByUserId,
-  createUserSearch,
-  updateUserSearch,
-  deleteUserSearch,
-} = require("./controllers/UserSearch");
-
-const {
-  createCancerData,
-  getCancerDataById,
-  deleteCancerData,
-} = require("./controllers/CancerData");
-
-const app = express();
-const { PORT = 4000 } = process.env;
-
-const origin =
-  process.env.NODE_ENV === "production"
-    ? process.env.PRODUCTION_CLIENT
-    : process.env.DEVELOPMENT_CLIENT;
-
-app.set('trust proxy', true)
-
-app.use(cors({ credentials: true, origin }));
+app.use(cors);
 app.use(express.json());
-app.use(cookieParser());
-app.use(
-  require("express-session")({
-    secret: process.env.SESSION_SECRET,
+
+/* app.use(
+  session({
     resave: false,
     saveUninitialized: true,
-    cookie: {sameSite: "none",
-            ...(process.env.NODE_ENV !== 'dev'
-        ? { domain: '.netlify.app' }
-        : {})},
+    secret: 'keyboard cat',
+    cookie: {sameSite: "none"},
     
   })
-);
+); */
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-console.log("x1")
+app.listen(port, () => {
+  console.log(`Test app listening at http://localhost:${port}`)
+})
 
-app.use((req, res, next) => {
-  console.log(req.session);
-  console.log(req.sessionID);
-  next();
-});
 
-app.use((err, req, res, next) => {
-  res.status(err.status).send(err);
-});
 
-app.get("/", (req, res) => {
-  req.session.requestCount = req.session.requestCount
-    ? req.session.requestCount + 1
-    : 1;
-  res.json(req.session);
-});
-
-app.post("/register", createUser);
-app.post("/login", loginUser);
-app.get("/logout", logoutUser);
-app.get("/profile", getUser);
-
-app.get("/user/usersearchs", getUserSearchByUserId);
-
-app.post("/usersearchs", createUserSearch);
-app.get("/usersearchs/:searchId", getUserSearchById);
-app.put("/usersearchs/:searchId", updateUserSearch);
-app.delete("/usersearchs/:searchId", deleteUserSearch);
-
-app.post("/cancerdata", createCancerData);
-app.get("/cancerdata/:cancerDataId", getCancerDataById);
-app.delete("/cancerdata/:cancerDataId", deleteCancerData);
-(async () => {
-  await connectDB();
-  app.listen(PORT, () =>
-    console.log(
-      colors.green.inverse(`api-make API running at http://localhost:${PORT}`)
-    )
-  );
-})();
